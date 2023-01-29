d131c7bb691e3299ed43f6d7fa82554de58bd63a
EduardoSilva09 <silvafeitosaeduardo71@gmail.com>
2023-01-29 16:06:24 -0300

    Desenvolvida strategia de autenticação com passaportLocal

---
+++ auth.js
@@ -0,0 +1,45 @@
+const bcrypt = require('bcryptjs')
+const LocalStrategy = require('passport-local').Strategy
+
+
+module.exports = function (passport) {
+    function findUser(username, callback) {
+        global.db.collection('users').findOne({ 'usename': username }, function (err, doc) {
+            callback(err, doc)
+        })
+    }
+
+    function findUserById(id, callback) {
+        const objectId = require('mongodb').ObjectId
+        global.db.collection('users').findOne({ '_id': objectId(id) }, function (err, doc) {
+            callback(err, doc)
+        })
+    }
+
+    passport.serializeUser(function (user, done) {
+        done(null, user._id)
+    })
+
+    passport.deserializeUser(function (user, done) {
+        findUserById(id, function (err, user) {
+            done(err, user)
+        })
+    })
+
+    passport.use(new LocalStrategy({
+        usernameField: 'username',
+        passwordField: 'password'
+    }, (username, password, done) => {
+        findUser(username, (err, user) => {
+            if (err) { return done(err) }
+
+            if (!user) { return done(null, false) }
+
+            bcrypt.compare(password, user.password, (err, isValid) => {
+                if (err) { return done(err) }
+                if (!isValid) { return done(null, false) }
+                return done(null, user)
+            })
+        })
+    }))
+}
