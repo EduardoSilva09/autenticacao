8f4e244c886b953a7950fdee08aba955d62291a6
EduardoSilva09 <silvafeitosaeduardo71@gmail.com>
2023-01-29 16:23:51 -0300

    desenvolvida rota de get e post em login

--- routes/login.js
+++ routes/login.js
@@ -17,4 +17,15 @@ router.get('/', function (req, res, next) {
     res.render('login', { title: 'Login', message: null })
 });
 
+router.get('/login', function (req, res, next) {
+    if (req.query.fail)
+        res.render('login', { message: 'Usuário e/ou senha incorretos!' })
+    else
+        res.render('login', { message: null })
+});
+
+router.post('/login',
+    passport.authenticate('local', { successRedirect: '/chat', failureRedirect: '/login?fail=true' })
+)
+
 module.exports = router;
